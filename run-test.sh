2ad95bb1ce1fc87f2b3bf04320a8f91a2243f328
Jan Mederer <jan@mederer.it>
2016-12-07 17:59:28 +0100

    create Report on every run

--- run-test.sh
+++ run-test.sh
@@ -3,7 +3,10 @@
 TESTSCRIPT=$1
 SERVERIP=$( ip addr show dev eth0 | awk  '/inet / {print $2}' | cut -d "/" -f 1)
 SLAVEIPS=$( /usr/local/bin/get-slaves-ip.sh )
+REPORTDIR=$( mktemp -d -p /logs)
+LOGFILE=${REPORTDIR}.log
 
 docker run --rm -it --name master -v /input-data:/input-data -v /logs/:/logs --rm -p 60000:60000 --name master confirmed/jmeter-master \
-    /var/lib/apache-jmeter/bin/jmeter -n -t /input-data/${TESTSCRIPT} \
-    -Djava.rmi.server.hostname=${SERVERIP} -Dclient.rmi.localport=60000 -R${SLAVEIPS}
+    /var/lib/apache-jmeter/bin/jmeter -n -t /input-data/${TESTSCRIPT} -l ${LOGFILE} -e -o ${REPORTDIR} \
+    -Djava.rmi.server.hostname=${SERVERIP} -Dclient.rmi.localport=60000 -R${SLAVEIPS} 
+
