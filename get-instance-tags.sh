28dc738811ab92f11470dcfe965a71a67d728fe2
Jan Mederer <jan@mederer.it>
2016-12-20 16:12:05 +0100

    update - get tag of local running instance

---
+++ get-instance-tags.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ -f /usr/bin/yum ]
+then
+  sudo yum install -y aws-cli
+fi
+
+INSTANCE_ID=$( curl --silent http://169.254.169.254/latest/meta-data/instance-id )
+REGION=$( curl --silent http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/.$//' )
+
+aws ec2 describe-tags --region $REGION --filter "Name=resource-id,Values=$INSTANCE_ID" --output=text | 
+    sed -r 's/TAGS\t(.*)\t.*\t.*\t(.*)/\1="\2"/'
+
