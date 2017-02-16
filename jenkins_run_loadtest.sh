47c71fe27ebc7d004bfb5c64ed0315462e471059
Jan Mederer <jan@mederer.it>
2017-02-16 22:12:28 +0100

    jenkins loadtest script

---
+++ jenkins_run_loadtest.sh
@@ -0,0 +1,48 @@
+mkdir -p data
+cd ./data
+unzip ../*.zip
+count=$( find . -type f -iname "*.jmx" | wc -l )
+
+echo "Setting permissons on input dir"
+sudo chown -R jenkins. /input-data/
+
+if [ $count -eq 1 ]; then
+  echo "all fine"  
+  jmx=$( find . -type f -iname "*.jmx" )  
+  jmxfile=$( basename $jmx )
+else
+  echo "error to much jmx files"  
+  exit 1
+fi
+
+find . -type f -exec cp -f {} /input-data \;
+
+export IFS=","
+for slave in $( get-slaves-ip.sh )
+do
+   ssh -i /var/lib/jenkins/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@${slave} "rm -rf /input-data/*"
+   scp -i /var/lib/jenkins/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r /input-data/* ubuntu@${slave}:/input-data/
+done
+unset IFS
+
+TESTSCRIPT=$jmxfile
+SERVERIP=$( ip addr show dev eth0 | awk  '/inet / {print $2}' | cut -d "/" -f 1)
+SLAVEIPS=$( /usr/local/bin/get-slaves-ip.sh )
+REPORTDIR=$( mktemp -d -p /logs)
+LOGFILE=${REPORTDIR}.log
+JTLFILE=${REPORTDIR}.jtl
+count=$( docker ps -a | wc -l )
+
+if [ $count -gt 1 ]
+then
+  docker rm master  
+  if [ $? -ne 0 ]  
+  then
+    echo "maybe some tests are still active? Can't remove the container"
+	exit 1  
+  fi
+fi
+
+docker run --rm  --name master -v /tmp/:/tmp/ -v /input-data:/input-data -v /logs/:/logs --rm -p 60000:60000 --name master confirmed/jmeter-master   /var/lib/apache-jmeter/bin/jmeter -n -t /input-data/${TESTSCRIPT} -l ${JTLFILE} -j ${LOGFILE} -e -o ${REPORTDIR} -Djava.rmi.server.hostname=${SERVERIP} -Dclient.rmi.localport=60000 -R${SLAVEIPS}
+
+sudo chmod -R 755 ${REPORTDIR}
