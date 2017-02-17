3d65bcea8b41abb107c4f5ed2d5a174bd3c1cf64
Jan Mederer <jan@mederer.it>
2017-02-17 09:47:29 +0100

    get-slave-ip give now optionally the public ips

--- get-slaves-ip.sh
+++ get-slaves-ip.sh
@@ -2,13 +2,25 @@
 
 zone=$( curl --silent http://169.254.169.254/latest/meta-data/placement/availability-zone )
 region=${zone%%[a-z]}
-as_group_name="${1:-as_hlp_jmeter_slave}"
+iptype="PrivateIpAddress"
+as_group_name="as_hlp_jmeter_slave"
+
+### GET List of the Paramters
+while (( "$#" )); do
+  if [ "$1" == "-p" ]; then
+    shift
+    iptype="PublicIpAddress"
+    continue
+  fi
+  as_group_name="${1}"
+  shift
+done
 
 instance_ids=$( aws --region ${region} \
                     --output text --query "AutoScalingGroups[0].Instances[*].InstanceId" autoscaling describe-auto-scaling-groups \
                     --auto-scaling-group-names ${as_group_name} )
 
-
 aws --region ${region} --output text  \
-    --query "Reservations[*].Instances[*].PrivateIpAddress" ec2 describe-instances \
+    --query "Reservations[*].Instances[*].${iptype}" ec2 describe-instances \
     --instance-ids ${instance_ids} | tr "\n" ","|  sed "s/\t/,/g" | sed "s/,$//"
+
