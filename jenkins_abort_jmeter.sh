ffd40ca62600521e3f3ebb89556f05d62f99e687
Jan Mederer <jan@mederer.it>
2017-02-17 09:55:32 +0100

    jenkins abort running tests

--- jenkins_abort_jmeter.sh
+++ jenkins_abort_jmeter.sh
@@ -6,6 +6,6 @@ docker rm master
 export IFS=","
 for server in $(get-slaves-ip.sh )
 do 
-  ssh -i /var/lib/jenkins/.ssh/id_rsa ubuntu@$server "docker restart slave"
+  ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /var/lib/jenkins/.ssh/id_rsa ubuntu@$server "docker restart slave"
 done
 
