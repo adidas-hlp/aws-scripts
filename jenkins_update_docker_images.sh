b128b4fd73aa45c9afd666c1be53085fb3db390d
Jan Mederer <jan@mederer.it>
2017-02-27 11:01:03 +0100

    update

--- jenkins_update_docker_images.sh
+++ jenkins_update_docker_images.sh
@@ -8,9 +8,9 @@ export IFS=","
 for slave in $( get-slaves-ip.sh )
 do
   # Slave
-  ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -l ubuntu $slave 'docker stop slav && docker rm slave  && docker pull confirmed/jmeter-slave &&
+  ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -l ubuntu $slave 'docker stop slave && docker rm slave  && docker pull confirmed/jmeter-slave &&
   docker run --restart=always -v /input-data:/input-data -w /input-data -dit  \
-    -e AWSINSTANCEIP=\$( ip addr show dev eth0 | awk  '/inet / {print $2}' | cut -d "/" -f 1) \
+    -e AWSINSTANCEIP=$( ip addr show dev eth0 | awk  \'/inet / {print $2}\' | cut -d "/" -f 1) \
     -p 1099:1099 -p 60000:60000 --name slave confirmed/jmeter-slave'
 done
 unset IFS
